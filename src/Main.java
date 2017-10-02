ee1daf730359309efc5a998c776c9149e51730d2
qqa00165 <31980610+qqa00165@users.noreply.github.com>
2017-10-02 23:21:37 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int number1,number2,number3,number4,number5,sum;
+        int number1,number2,number3,number4,sum;
         System.out.print("a:");
         number1=input.nextInt();
         System.out.print("x:");
         number2=input.nextInt();
         System.out.print("b:");
         number3=input.nextInt();
-        System.out.print("x:");
-        number4=input.nextInt();
         System.out.print("c:");
-        number5=input.nextInt();
-        sum=number1*(number2*number2)+number3*number4+number5;
+        number4=input.nextInt();
+        sum=number1*(number2*number2)+number3*number4+number2;
         System.out.printf("計算結果=%d",sum);
         input.close();
 
